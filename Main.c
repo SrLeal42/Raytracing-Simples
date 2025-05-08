2de68cf8080b9e2427327a4767760dc801f75f26
SrLeal42 <gabriellealjoao@gmail.com>
2025-05-07 23:55:38 -0300

    ADD: Luz
    Adicionando luz que dispensa com a distancia

--- Main.c
+++ Main.c
@@ -12,7 +12,7 @@
 #define RGB_AMARELO = {255,255,0}
 #define RGB_LARANJA = {255,140,0}
 #define NUM_CIRCULOS_OPACOS 2
-#define NUM_CIRCULOS_BRILHANTES 2
+#define NUM_CIRCULOS_BRILHANTES 1
 
 struct pixel {
     double x;
@@ -88,7 +88,7 @@ void DrawCircle(struct pixel* arrayPixels, struct Circle circle){
 
         }
     }
-
+    
 }
 
 
@@ -99,16 +99,52 @@ void DrawCircles(struct pixel* arrayPixels, struct Circle circles[], int num_cir
 }
 
 
-void Light(SDL_Surface* surface, struct Circle op_circles[], struct Circle br_circles[]){
+void Light(struct pixel* arrayPixels, struct Circle op_circles[], struct Circle br_circles[]){
 
-    for (int y = 0; y <= ALTURA; y++){
-        for (int x = 0; x <= LARGURA; x++){
-            SDL_Rect p = (SDL_Rect) {x, y, 1, 1};
-            Uint32 cor = SDL_MapRGB(SDL_GetPixelFormatDetails(surface->format), NULL, 255, 255, 0);
-            SDL_FillSurfaceRect(surface, &p, cor);
+
+    for (int y = 0; y < ALTURA; y++){
+        for (int x = 0; x < LARGURA; x++){
+            
+            int idx = y * LARGURA + x;
+
+            struct pixel* pxl = &arrayPixels[idx]; 
+
+            double pxl_alpha = 0.0; 
+            
+            for (int c = 0; c < NUM_CIRCULOS_BRILHANTES; c++){
+                
+                double distX = x - br_circles[c].x;
+                double distY = y - br_circles[c].y;
+                double dist = pow(pow(distX, 2) + pow(distY, 2), 0.5); // Elevando a 0.5 que é mesma coisa que fazer a raiz quadrada
+
+                // printf("idx: %d  x: %d  y: %d  a: %.2f\n", idx, x, y, pxl->a);
+                double a = 0.0;
+                a = dist <= br_circles[c].r ? 100.0 : 100 * pow(.997, dist);
+
+
+                // if (dist != 0 ){
+                //     // a = 100/(dist * 0.01);
+                    
+                // }
+
+                if (a > pxl_alpha){
+                    pxl_alpha = a;
+                }
+
+            }
+            
+            pxl_alpha = pxl_alpha <= 0 ? 0.0 : pxl_alpha;
+            // printf("idx: %d AA: %.2lf\n", idx, pxl_alpha);
+            pxl->a = pxl_alpha;
+            
+
+
+            // SDL_Rect p = (SDL_Rect) {x, y, 1, 1};
+            // Uint32 cor = SDL_MapRGB(SDL_GetPixelFormatDetails(surface->format), NULL, 255, 255, 0);
+            // SDL_FillSurfaceRect(surface, &p, cor);
         }
     }
-
+        
 }
 
 
@@ -123,7 +159,7 @@ void DrawScreenPixels(SDL_Surface* surface, struct pixel* arrayPixels){
 
             SDL_Rect p = (SDL_Rect) {j, i, 1, 1};
             
-            Uint32 cor = SDL_MapRGB(SDL_GetPixelFormatDetails(surface->format), NULL, pxl.r, pxl.g, pxl.b);
+            Uint32 cor = SDL_MapRGB(SDL_GetPixelFormatDetails(surface->format), NULL, pxl.r * (pxl.a * 0.01), pxl.g * (pxl.a * 0.01), pxl.b * (pxl.a * 0.01));
             // Uint32 cor = SDL_MapRGB(SDL_GetPixelFormatDetails(surface->format), NULL, 255, 255, 255);
 
             SDL_FillSurfaceRect(surface, &p, cor);
@@ -146,7 +182,7 @@ int main(){
     };
 
     struct Circle bright_circles[NUM_CIRCULOS_BRILHANTES] = {
-        (struct Circle) {200, 200, 50, {255,140,0}}
+        (struct Circle) {400, 200, 50, {255,140,0}}
     };
 
 
@@ -159,7 +195,7 @@ int main(){
     SDL_Event event;
 
     while(running){
-        
+
         while(SDL_PollEvent(&event)){
             if (event.type == SDL_EVENT_QUIT){
                 running = false;
@@ -167,10 +203,11 @@ int main(){
         }
 
 
-        // Light(surface, opaque_circles, bright_circles);
+        
 
         DrawCircles(ScreenPixels, opaque_circles, NUM_CIRCULOS_OPACOS);
-        DrawCircles(ScreenPixels, bright_circles, NUM_CIRCULOS_BRILHANTES);    
+        DrawCircles(ScreenPixels, bright_circles, NUM_CIRCULOS_BRILHANTES);
+        Light(ScreenPixels, opaque_circles, bright_circles);
         
         DrawScreenPixels(surface, ScreenPixels);
 
